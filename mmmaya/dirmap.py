9b6e839f2ac39a8394cb14b8379df6945b946b25
Mike Boers <mikeb@markmedia.co>
2017-02-27 11:15:46 -0500

    Add another dirmap for Mike's SitG.

--- mmmaya/dirmap.py
+++ mmmaya/dirmap.py
@@ -2,22 +2,24 @@ from maya import cmds
 
 
 def setup():
-	print '[mmmaya.dirmap] Setting up dirmaps:'
-	for src, dst in [
+    print '[mmmaya.dirmap] Setting up dirmaps:'
+    for src, dst in [
 
-		# Windows to Linux.
-		('K:/', '/Volumes/CGroot'),
+        # Windows to Linux.
+        ('K:/', '/Volumes/CGroot'),
                 ('Y:/', '/Volumes/PD01'),
-		('Z:/', '/Volumes/AnimationProjects'),
+        ('Z:/', '/Volumes/AnimationProjects'),
 
-		# Old and new (July 4th) Miao Miao.
-		('//10.10.1.3/AnimationProjects', '/Volumes/AnimationProjects'),
-		('//10.10.1.5/AnimationProjects', '/Volumes/AnimationProjects'),
+        # Old and new (2016-07-04) Miao Miao.
+        ('//10.10.1.3/AnimationProjects', '/Volumes/AnimationProjects'),
+        ('//10.10.1.5/AnimationProjects', '/Volumes/AnimationProjects'),
 
-                # SitG: Mike's home to studio.
-                ('/Volumes/heap/sitg/work/markmedia', '/Volumes/CGroot/Projects/SitG'),
+        # SitG: Mike's home to studio.
+        ('/Volumes/heap/sitg/work/markmedia', '/Volumes/CGroot/Projects/SitG'), # Old.
+        ('/Volumes/heap/sitg/work/film',      '/Volumes/CGroot/Projects/SitG'),
 
-	]:
-		print '[mmmaya.dirmap]     Mapping %s to %s' % (src, dst)
-		cmds.dirmap(m=(src, dst))
-	cmds.dirmap(enable=True)
+    ]:
+        print '[mmmaya.dirmap]     Mapping %s to %s' % (src, dst)
+        cmds.dirmap(m=(src, dst))
+       
+    cmds.dirmap(enable=True)
