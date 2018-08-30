7b6cbe065916ec3cccee3ddc4ae75b4383f2431a
Mike Boers <mikeb@loftysky.com>
2018-08-30 15:33:31 -0400

    Link via parent/scale constraints, and clean everything back up.

--- mmmaya/abc/link.py
+++ mmmaya/abc/link.py
@@ -10,7 +10,7 @@ def collect_by_name(root_node, res=None, **kwargs):
     res = {}
 
     for node in cmds.listRelatives(root_node, allDescendents=True, fullPath=True, **kwargs):
-        print node
+        #print node
         res.setdefault(_name(node), node)
 
     return res
@@ -30,42 +30,72 @@ def link_button():
     dsts = collect_by_name(sel[1], type='mesh')
 
     for name, src_node in sorted(srcs.iteritems()):
+        
         dst_node = dsts.get(name)
         if not dst_node:
             continue
 
-        print name, src_node, dst_node
+        # Only bother transferring attributes on meshes that have a connection
+        # to an alembic node. This isn't a very strong connection, but it
+        # seems to work for us.
+        is_deformed = False
+        for node in cmds.listHistory(src_node) or ():
+            type_ = cmds.nodeType(node)
+            if type_ == 'AlembicNode':
+                is_deformed = True
+                break
+        if not is_deformed:
+            continue
+
+        print 'mesh', name
+        print '    src node:', src_node
+        print '    dst node:', dst_node
 
         src_transform = cmds.listRelatives(src_node, parent=True, path=True)[0]
         dst_transform = cmds.listRelatives(dst_node, parent=True, path=True)[0]
 
-        # TODO: Check that src is even deforming first.
         transfer = cmds.transferAttributes(src_transform, dst_transform,
-            afterReference=1,
+            afterReference=1, # To play nice with references.
             transferPositions=1,
             transferNormals=1,
             sampleSpace=4, # 4 == components
         )[0]
 
-        print '   ', transfer
+        print '    transferAttributes:', transfer
 
 
     srcs = collect_by_name(sel[0], type='transform')
     dsts = collect_by_name(sel[1], type='transform')
 
     for name, src_node in sorted(srcs.iteritems()):
+
         dst_node = dsts.get(name)
         if not dst_node:
             continue
 
-        print name, src_node, dst_node
+        print 'transform', name
+        print '    src node:', src_node
+        print '    dst node:', dst_node
 
-        # TODO: Do this with contraints.
-        for attr in ('translate', 'rotate', 'scale', 'visibility'):
-            connection = cmds.connectAttr(src_node + '.' + attr, dst_node + '.' + attr, force=True) 
 
-        print '   ', connection
+        is_animated = False
+        for node in cmds.listConnections(src_node, skipConversionNodes=True, source=True, destination=False) or ():
+            if cmds.nodeType(node) == 'AlembicNode':
+                is_animated = True
+                break
 
+        if is_animated:
+            # Bulk of transform is handled by a parent constraint...
+            constraint = cmds.parentConstraint(src_node, dst_node, name='abc_link_parent_' + name)[0]
+            print '    parentConstraint:', constraint
+            constraint = cmds.scaleConstraint(src_node, dst_node, name='abc_link_scale_' + name)[0]
+            print '    scaleConstraint:', constraint
+
+        if name != 'hi':
+            # ... and other attributed are done directly.
+            for attr_name in ('visibility', ):
+                cmds.connectAttr(src_node + '.' + attr_name, dst_node + '.' + attr_name, force=True)
+                print '    {}: {} -> {}'.format(attr_name, src_node, dst_node)
 
 
 def unlink_button():
@@ -77,27 +107,57 @@ def unlink_button():
 
 
     for mesh in cmds.listRelatives(root, allDescendents=True, fullPath=True, type='mesh'):
-        print mesh
+        
+        print cmds.nodeType(mesh), mesh
 
         for node in cmds.listHistory(mesh):
+        
+            # At some point this didn't work for us...
             try:
                 type_ = cmds.nodeType(node)
             except RuntimeError:
+                print '    ** Could not get type of', node
                 continue
+
             if type_ != 'transferAttributes':
                 continue
-            print '   ', node
+            print '    transferAttributes:', node
+
             cmds.delete(node)
 
-    for xform in cmds.listRelatives(root, allDescendents=True, fullPath=True, type='transform'):
-        print xform
+    for xform in cmds.listRelatives(root, allDescendents=True, fullPath=True, type='transform') or ():
 
-        for src_attr in cmds.listConnections(xform, source=True, destination=False, plugs=True) or ():
+        # Even though we ask for transforms, we get constraints, because
+        # constraints inherit from transforms.
+        if cmds.nodeType(xform) != 'transform':
+            continue
 
-            attr_name = src_attr.split('.')[-1]
+        print 'transform', xform
+
+        # Remove the parent constraints.
+        for constraint_type in ('parentConstraint', 'scaleConstraint'):
+            for node in set(cmds.listConnections(xform, source=True, destination=False, type=constraint_type) or ()):
+                print '    {}: {}'.format(constraint_type, node)
+                cmds.delete(node)
+
+        # Remove attribute connections.
+        connections = cmds.listConnections(xform, source=True, destination=False, type='transform', plugs=True, connections=True) or ()
+        for i in xrange(0, len(connections), 2):
+
+            dst_attr = connections[i] # The given object is returned first.
+            src_attr = connections[i + 1]
+
+            src_node, attr_name  = src_attr.split('.')
+            dst_node, attr_name2 = dst_attr.split('.')
+            if attr_name != attr_name2:
+                continue
+
+            # We remove a few too many connections here, because the development
+            # versions of this transfered transforms via attribute connections.
             if attr_name not in ('translate', 'rotate', 'scale', 'visibility'):
                 continue
 
+            print '    {}: {} -> {}'.format(attr_name, src_node, dst_attr.split('.')[0])
             cmds.disconnectAttr(src_attr, xform + '.' + attr_name)
 
 
